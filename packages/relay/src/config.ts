cf6bdc5affccb3967547a13d58847b38ad4ad62b
Evis Chang <yschang0926@outlook.com>
2023-06-27 09:34:41 +0800

    add config

---
+++ packages/relay/src/config.ts
@@ -0,0 +1,26 @@
+import { ethers } from 'ethers'
+import _config from '../../config'
+import { config } from 'dotenv'
+config()
+
+export const UNIREP_ADDRESS =
+    process.env.UNIREP_ADDRESS ?? _config.UNIREP_ADDRESS
+export const APP_ADDRESS = process.env.APP_ADDRESS ?? _config.APP_ADDRESS
+export const ETH_PROVIDER_URL =
+    process.env.ETH_PROVIDER_URL ?? _config.ETH_PROVIDER_URL
+export const PRIVATE_KEY = process.env.PRIVATE_KEY ?? _config.PRIVATE_KEY
+
+export const DB_PATH = process.env.DB_PATH ?? ':memory:'
+
+export const provider = ETH_PROVIDER_URL.startsWith('http')
+    ? new ethers.providers.JsonRpcProvider(ETH_PROVIDER_URL)
+    : new ethers.providers.WebSocketProvider(ETH_PROVIDER_URL)
+
+export const CALLBACK_URL = process.env.CALLBACK_URL ?? 'http://127.0.0.1:8000/api/callback'
+
+// twitter related settings
+export const TWITTER_ACCESS_TOKEN_URL = process.env.TWITTER_ACCESS_TOKEN_URL ?? 'https://api.twitter.com/2/oauth2/token'
+export const TWITTER_USER_URL = process.env.TWITTER_USER_URL ?? 'https://api.twitter.com/2/users/me'
+
+export const TWITTER_CLIENT_ID = process.env.TWITTER_CLIENT_ID
+export const TWITTER_CLIENT_KEY = process.env.TWITTER_CLIENT_KEY
