404eb03d5bf33114d9c205d28f1cf05edd7cc928
ott <ott@Junos-MacBook-Air.local>
2023-07-01 17:48:58 -0400

    fix, format test

--- packages/contracts/test/UnirepApp.test.ts
+++ packages/contracts/test/UnirepApp.test.ts
@@ -60,34 +60,34 @@ describe('Unirep App', function () {
         userState.sync.stop()
     })
 
-    describe('user post', ()=>{
-        it('post with valid proof', async()=>{
+    describe('user post', () => {
+        it('post with valid proof', async () => {
             const userState = await genUserState(id, app)
-            const { publicSignals, proof } =
-                await userState.genEpochKeyProof()
-            const content = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("testing"))
+            const { publicSignals, proof } = await userState.genEpochKeyProof()
+            const content = ethers.utils.keccak256(
+                ethers.utils.toUtf8Bytes('testing')
+            )
             expect(app.post(publicSignals, proof, content))
-            .to.emit(app, "Post")
-            .withArgs(publicSignals[0], 0, content);
+                .to.emit(app, 'Post')
+                .withArgs(publicSignals[0], 0, content)
         })
 
-        it('revert post with reused proof', async()=>{
+        it('revert post with reused proof', async () => {
             const userState = await genUserState(id, app)
-            const { publicSignals, proof } =
-                await userState.genEpochKeyProof()
-            const content = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("testing"))
-            
-            expect(app.post(publicSignals, proof, content))
-            .to.emit(app, "Post")
-            .withArgs(publicSignals[0], 0, content);
+            const { publicSignals, proof } = await userState.genEpochKeyProof()
+            const content = ethers.utils.keccak256(
+                ethers.utils.toUtf8Bytes('testing')
+            )
 
             expect(app.post(publicSignals, proof, content))
-            .to.be.revertedWith("The proof has been used before") 
+                .to.emit(app, 'Post')
+                .withArgs(publicSignals[0], 1, content)
+
+            expect(app.post(publicSignals, proof, content)).to.be.revertedWith(
+                'The proof has been used before'
+            )
         })
-
     })
-
-    
 
     it('submit attestations', async () => {
         const userState = await genUserState(id, app)
