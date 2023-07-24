e5e7b3986fadacc6d24506086564c6da017c6534
andy78644 <andy690100391@gmail.com>
2023-07-24 20:31:54 +0800

    Add IPFS client for post content

--- packages/relay/src/singletons/schema.ts
+++ packages/relay/src/singletons/schema.ts
@@ -31,6 +31,7 @@ const _schema = [
             ['postId', 'String', { optional: true }],
             ['transactionHash', 'String', { optional: true }],
             ['content', 'String', { optional: true }],
+            ['cid', 'String', { optional: true }],
             ['epoch', 'Int'],
             ['epochKey', 'String'],
             {
