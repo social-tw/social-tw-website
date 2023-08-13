8c68ec30d3a1dbd0338e3ef23293d47903704345
HSIAO PO WEN <xiawpohr@gmail.com>
2023-08-13 10:56:43 +0800

    feat: add a dialog for failing to create a post

--- packages/frontend/src/pages/PostCreate.tsx
+++ packages/frontend/src/pages/PostCreate.tsx
@@ -1,9 +1,13 @@
+import { useRef } from 'react'
 import { toast } from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
+import Dialog from '../components/Dialog'
 import PostForm, { PostValues } from '../components/PostForm'
 import usePosts from '../hooks/usePosts'
 
 export default function PostCreate() {
+    const errorDialog = useRef<HTMLDialogElement>(null)
+
     const navigate = useNavigate()
 
     const { create } = usePosts()
@@ -14,13 +18,31 @@ export default function PostCreate() {
             toast('貼文成功送出')
             navigate('/')
         } catch (err) {
-            toast((err as Error).message)
+            errorDialog?.current?.showModal()
         }
     }
 
     return (
         <div className="p-4">
             <PostForm onCancel={() => navigate('/')} onSubmit={onSubmit} />
+            <Dialog ref={errorDialog}>
+                <section className="p-6 md:px-12">
+                    <p className="text-base font-medium text-black/90">
+                        親愛的用戶：
+                        <br />
+                        <br />
+                        很抱歉通知您，我們注意到您的貼文發布時似乎遇到了網路連線不穩定的情況，導致發文失敗。我們深感抱歉給您帶來的不便。請您再次嘗試發佈文章{' '}
+                        <br />
+                        <br />
+                        感謝您的理解與合作。
+                    </p>
+                </section>
+                <section className="flex justify-center p-6 md:p-12 md:pt-0">
+                    <button className="max-w-[285px] w-full h-14 rounded-lg bg-primary/90 text-white/90 flex justify-center items-center text-xl font-bold tracking-[30%]">
+                        重新發佈
+                    </button>
+                </section>
+            </Dialog>
         </div>
     )
 }
--- packages/frontend/src/pages/PostList.tsx
+++ packages/frontend/src/pages/PostList.tsx
@@ -1,8 +1,9 @@
 import clsx from 'clsx'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import toast from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
 import { useMediaQuery } from '@uidotdev/usehooks'
+import Dialog from '../components/Dialog'
 import Post from '../components/Post'
 import PostForm, { PostValues } from '../components/PostForm'
 import { SERVER } from '../config'
@@ -52,6 +53,8 @@ const examplePosts = [
 ]
 
 export default function PostList() {
+    const errorDialog = useRef<HTMLDialogElement>(null)
+
     const [posts, setPosts] = useState<Post[]>([])
 
     useEffect(() => {
@@ -73,7 +76,7 @@ export default function PostList() {
             await create(values.content)
             toast('貼文成功送出')
         } catch (err) {
-            toast((err as Error).message)
+            errorDialog?.current?.showModal()
         }
     }
 
@@ -107,6 +110,24 @@ export default function PostList() {
                     ))}
                 </ul>
             </section>
+            <Dialog ref={errorDialog}>
+                <section className="p-6 md:px-12">
+                    <p className="text-base font-medium text-black/90">
+                        親愛的用戶：
+                        <br />
+                        <br />
+                        很抱歉通知您，我們注意到您的貼文發布時似乎遇到了網路連線不穩定的情況，導致發文失敗。我們深感抱歉給您帶來的不便。請您再次嘗試發佈文章{' '}
+                        <br />
+                        <br />
+                        感謝您的理解與合作。
+                    </p>
+                </section>
+                <section className="flex justify-center p-6 md:p-12 md:pt-0">
+                    <button className="max-w-[285px] w-full h-14 rounded-lg bg-primary/90 text-white/90 flex justify-center items-center text-xl font-bold tracking-[30%]">
+                        重新發佈
+                    </button>
+                </section>
+            </Dialog>
         </div>
     )
 }
