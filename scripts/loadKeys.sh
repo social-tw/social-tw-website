76b72d809451a590e872ec2fb4e92dc5082569b2
ryanycw <ryan@ryanycw.dev>
2023-08-21 01:16:14 +0800

    chore: update circuit key and checksum

--- scripts/loadKeys.sh
+++ scripts/loadKeys.sh
@@ -5,9 +5,9 @@ set -e
 WORKDIR=$(mktemp)
 TARFILE=$WORKDIR/keys.tar.gz
 
-wget https://keys.unirep.io/2-beta-1/keys.tar.gz -P $WORKDIR --progress=bar:force:noscroll
+wget https://keys.unirep.io/2-beta-3/keys.tar.gz -P $WORKDIR --progress=bar:force:noscroll
 
-shasum -a 256 $TARFILE | grep '99891da0525f617ba382e18e2cc6c9b55d45b55711dd789403d31b3945e44e34'
+shasum -a 256 $TARFILE | grep '674b77478c59a02f58cdf3a4090bb6db415adbd75a826a472fa4321145b672fb'
 
 tar -xzf $TARFILE -C packages/relay
 mv packages/relay/zksnarkBuild packages/relay/keys
