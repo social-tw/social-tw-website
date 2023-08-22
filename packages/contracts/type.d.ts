b0ec7dd044fb198f025b68d40b5b3345ecc4a680
Fu-Chuan Chung <sh1001309@gmail.com>
2023-08-22 21:04:18 +0800

    chore: added devDep/@nomiclabs/hardhat-waffle to avoid chai's assertion warning

---
+++ packages/contracts/type.d.ts
@@ -0,0 +1,18 @@
+import { Contract } from "ethers";
+
+declare global {
+  namespace Chai {
+    interface Assertion {
+      reverted: Assertion;
+      revertedWith(reason: string): Promise<void>;
+      emit(contract: Contract, eventName: string): EmitMatcher;
+    }
+
+    interface EmitMatcher {
+      withArgs(...args: any[]): EmitMatcher;
+      from(address: string): Assertion;
+      atLeastOnce(): Assertion;
+      exactly(times: number): Assertion;
+    }
+  }
+}
