9c5a949ca66cfa43a46c72eef6532711ee82b0ae
badukwei <linwgpeter@gmail.com>
2023-08-22 20:57:32 +0800

    prettier

--- packages/frontend/src/pages/Login.tsx
+++ packages/frontend/src/pages/Login.tsx
@@ -5,6 +5,7 @@ import DemoPostList from '../components/login/DemoPostList'
 import { clsx } from 'clsx'
 import { useSearchParams } from 'react-router-dom'
 import { IoChevronBack } from 'react-icons/io5'
+import StepInfo from '../components/login/StepInfo'
 
 const Login: React.FC = () => {
     const [searchParams] = useSearchParams()
@@ -89,58 +90,11 @@ const Login: React.FC = () => {
                             initial='hidden'
                             animate='visible'
                         >
-                            <div className='flex w-[80px] flex-col justify-center items-center gap-2'>
-                                <div
-                                    className={clsx(`
-                                    text-white 
-                                    w-[60px] 
-                                    h-[60px] 
-                                    flex 
-                                    items-center 
-                                    justify-center 
-                                    font-semibold 
-                                    bg-[#FF892A] 
-                                    rounded-full 
-                                    text-[30px]
-                                    `,
-                                        hashUserId && 'border-[5px] border-white'
-                                    )}
-                                >1</div>
-                                <div className='text-white text-sm flex flex-col justify-center items-center tracking-wide'>
-                                    <span>Twitter</span>
-                                    <span>帳號登入</span>
-                                </div>
-                            </div>
-                            <div className='w-[50px] mb-10 flex justify-between items-center'>
-                                <span className='bg-[#FF892A] h-1 w-1'></span>
-                                <span className='bg-[#FF892A] h-1 w-2'></span>
-                                <span className='bg-[#FF892A] h-1 w-2'></span>
-                                <span className='bg-[#FF892A] h-1 w-1'></span>
-                            </div>
-                            <div
-                                className={clsx(
-                                    `flex w-[80px] flex-col justify-center items-center gap-2`,
-                                    !hashUserId && 'opacity-30'
-                                )}>
-                                <div
-                                    className='
-                                    text-white 
-                                    w-[60px] 
-                                    h-[60px] 
-                                    flex 
-                                    items-center 
-                                    justify-center 
-                                    font-semibold 
-                                    bg-[#FF892A] 
-                                    rounded-full 
-                                    text-[30px]
-                                    '
-                                >2</div>
-                                <div className='text-white text-sm flex flex-col justify-center items-center tracking-wide'>
-                                    <span>錢包註冊</span>
-                                    <span>Or 直接註冊</span>
-                                </div>
-                            </div>
+                            {
+                                <StepInfo
+                                    hashUserId={hashUserId}
+                                />
+                            }
                         </motion.div>
                     }
                 </div>
