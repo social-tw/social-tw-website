6e4e27c3257d420af30ba79d974d965f6f4f66e9
nooma <suncloud2203357667@gmail.com>
2023-08-23 08:39:22 +0800

    change config hardcoded in align with original

--- packages/relay/test/configs.ts
+++ packages/relay/test/configs.ts
@@ -1,17 +1,5 @@
-import getPort from 'get-port'
+const epochLength = 300;
+const PORT = parseInt(process.env.PORT ?? '8000', 10);
+const HTTP_SERVER = `http://127.0.0.1:${PORT}`;
 
-let PORT: number
-let HTTP_SERVER: string
-
-const epochLength = 300
-
-;(async () => {
-    const dynamicPort: number = await getPort()
-    process.env.PORT = dynamicPort.toString()
-    process.env.HTTP_SERVER = `http://127.0.0.1:${process.env.PORT}`
-
-    PORT = parseInt(process.env.PORT ?? '8000', 10)
-    HTTP_SERVER = `http://127.0.0.1:${PORT}`
-})()
-
-export { PORT, HTTP_SERVER, epochLength }
+export { PORT, HTTP_SERVER, epochLength };
