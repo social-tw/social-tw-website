d165da26bfd51c617c36de7d53e87671cb91ba47
MorrisLin <aka611511@gmail.com>
2023-08-24 21:52:44 +0800

    feat: merge from post e2e test

---
+++ packages/frontend/jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    "@testing-library/react": "<rootDir>/node_modules/@testing-library/react",
+    "@types/jest": "jest"
+  },
+};
