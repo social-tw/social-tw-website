df8d839c4f0d257dd83eaf2b0753baa5376c7a1c
Fu-Chuan Chung <sh1001309@gmail.com>
2023-08-24 21:25:04 +0800

    chore: deal with errors from conflicts

--- packages/contracts/contracts/UnirepApp.sol
+++ packages/contracts/contracts/UnirepApp.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 import {Unirep} from '@unirep/contracts/Unirep.sol';
 import {EpochKeyVerifierHelper} from '@unirep/contracts/verifierHelpers/EpochKeyVerifierHelper.sol';
-import {EpochKeyVerifierHelper} from '@unirep/contracts/verifierHelpers/EpochKeyVerifierHelper.sol';
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
