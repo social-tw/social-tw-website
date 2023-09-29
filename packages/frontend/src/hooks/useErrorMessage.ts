a1a5eff8696c0ad74265a100bc11f6d6faf0e898
badukwei <linwgpeter@gmail.com>
2023-09-29 15:50:11 +0800

    fix: remove console logs

--- packages/frontend/src/hooks/useErrorMessage.ts
+++ packages/frontend/src/hooks/useErrorMessage.ts
@@ -3,7 +3,6 @@ import ERROR_MESSAGES from '../constants/error-messages/loginErrorMessage'
 
 const useErrorMessage = (errorCode: keyof typeof ERROR_MESSAGES | '') => {
     const errorMessage = useMemo(() => {
-        console.log(errorCode)
         if (!errorCode) return { code: 'NO_ERROR', message: '' }
         return ERROR_MESSAGES[errorCode]
     }, [errorCode])
