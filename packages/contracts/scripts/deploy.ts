2adf4c9709b1ba39939e7255a7e70d6df1e232d2
Tihe <tihechen@gmail.com>
2023-10-10 00:01:00 +0800

    install circom on frontend dockerfile

--- packages/contracts/scripts/deploy.ts
+++ packages/contracts/scripts/deploy.ts
@@ -15,25 +15,25 @@ main().catch((err) => {
 export async function main() {
     const [signer] = await ethers.getSigners()
     const { unirep, app } = await deployApp(signer, epochLength)
-    
+
     const configPath = path.join(__dirname, '../../relay/.env')
 
-    fs.readFile(configPath,'utf8', async (err,  data) => {
+    fs.readFile(configPath, 'utf8', async (err, data) => {
         if (err) {
-            console.error(err);
-            return;
-          }
-          const config = envfile.parse(data)
-          
-          config.UNIREP_ADDRESS = unirep.address
-          config.APP_ADDRESS = app.address
-          config.ETH_PROVIDER_URL = hardhat.network.config.url ?? ''
-          config.PRIVATE_KEY = Array.isArray(hardhat.network.config.accounts)
-                  ? `${hardhat.network.config.accounts[0]}'`
-                  : `/**
+            console.error(err)
+            return
+        }
+        const config = envfile.parse(data)
+
+        config.UNIREP_ADDRESS = unirep.address
+        config.APP_ADDRESS = app.address
+        config.ETH_PROVIDER_URL = hardhat.network.config.url ?? ''
+        config.PRIVATE_KEY = Array.isArray(hardhat.network.config.accounts)
+            ? `${hardhat.network.config.accounts[0]}'`
+            : `/**
               This contract was deployed using a mnemonic. The PRIVATE_KEY variable needs to be set manually
               **/`
-          await fs.promises.writeFile(configPath, envfile.stringify(config));
+        await fs.promises.writeFile(configPath, envfile.stringify(config))
     })
 
     console.log(`Config written to ${configPath}`)
