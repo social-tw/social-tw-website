d0a67a01b3b8a48ccdd4883c417fa636b608e97c
Evis Chang <yschang0926@outlook.com>
2023-10-11 22:37:24 +0800

    Modify the ci flow
    add to push
    
    add oauth
    
    minor modify

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -15,6 +15,4 @@ function getServer(env: string): string {
     return server
 }
 
-// export const SERVER = 'https://relay.demo.unirep.io'
-export const KEY_SERVER = 'http://localhost:8000/build/'
-// export const KEY_SERVER = 'https://keys.unirep.io/2-beta-1/'
+export const KEY_SERVER = `${getServer(process.env.ENV!)}/build/`
