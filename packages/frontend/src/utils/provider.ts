a336148a11e15287e3bab32bcd7302e6b5dc1a1e
Dylan Yang <dogi9527dogi9527@gmail.com>
2023-10-22 11:21:31 +0800

    feat: impl createProviderByUrl

---
+++ packages/frontend/src/utils/provider.ts
@@ -0,0 +1,8 @@
+import { ethers } from 'ethers'
+
+export function createProviderByUrl(url: string) {
+    const SCHEMA = 'http'
+    return url.startsWith(SCHEMA)
+        ? new ethers.providers.JsonRpcProvider(url)
+        : new ethers.providers.WebSocketProvider(url)
+}
