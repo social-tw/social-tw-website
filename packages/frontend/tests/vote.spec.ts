b2f5d1b49682d7643fc89708a824c444a6e16f0e
chen-yanlong <alan900801@gmail.com>
2023-11-06 21:20:33 +0800

    feat: initial file

---
+++ packages/frontend/tests/vote.spec.ts
@@ -0,0 +1,52 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('had not upvoted', () => {
+    test('should choose post correctly', () => {
+
+    })
+    test('should be able to upvote and update the vote count correctly', () => {
+
+    })
+    test('should handle error messages and return to the correct status', () => {
+
+    })
+})
+
+test.describe('had not downvoted', () => {
+    test('should choose post correctly', () => {
+
+    })
+    test('should be able to downvote and update the vote count correctly', () => {
+
+    })
+    test('should handle error messages and return to the correct status', () => {
+        
+    })
+})
+
+test.describe('had upvoted', () => {
+    test('should be able to cancel and update the vote count correctly', () => {
+
+    })
+
+    test('should be able to upvote after canceled', () => {
+
+    })
+    test('should handle error messages and return to the correct status', () => {
+        
+    })
+})
+
+test.describe('had downvoted', () => {
+    test('should be able to cancel', () => {
+        
+    })
+
+    test('should be able to downvote after canceled', () => {
+
+    })
+    test('should handle error messages and return to the correct status', () => {
+        
+    })
+
+})
