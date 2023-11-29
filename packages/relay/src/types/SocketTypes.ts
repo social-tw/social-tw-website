0cbb447c72a3ad0b0b23461e3487ce1c4f3703bd
MorrisLin <aka611511@gmail.com>
2023-11-29 22:27:24 +0800

    feat: fix SocketTypes merge error

--- packages/relay/src/types/SocketTypes.ts
+++ packages/relay/src/types/SocketTypes.ts
@@ -5,6 +5,11 @@ export enum EventType {
     COMMENT = 'COMMENT',
 }
 
+export enum RoomType {
+    VOTE = 'VOTE',
+    COMMENT = 'COMMENT',
+}
+
 export interface VoteMsg {
     postId: string
     epoch: number
