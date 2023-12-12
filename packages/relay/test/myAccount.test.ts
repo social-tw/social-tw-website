62ecbf9ac12056731f0d0fafaa3a8910faae6c85
Jason <h044351528@gmail.com>
2023-12-13 03:10:38 +0800

    Implement backend APIs for my account page

---
+++ packages/relay/test/myAccount.test.ts
@@ -0,0 +1,55 @@
+import fetch from 'node-fetch'
+import { ethers } from 'hardhat'
+import { expect } from 'chai'
+
+import { HTTP_SERVER } from './configs'
+import { deployContracts, startServer } from './environment'
+
+import { Server } from 'http'
+import { addActionCount } from '../src/utils/TransactionHelper'
+
+describe('My Account Page', function () {
+    let snapshot: any
+    let express: Server
+    let postEpochKey = 'post-epoch-key'
+    let commentEpochKey = 'comment-epoch-key'
+    before(async function () {
+        snapshot = await ethers.provider.send('evm_snapshot', [])
+        // deploy contracts
+        const { unirep, app } = await deployContracts(100000)
+        // start server
+        const { db, prover, provider, synchronizer, server } =
+            await startServer(unirep, app)
+        express = server
+        const epoch = 1
+
+        // insert mock post
+        await addActionCount(db, postEpochKey, epoch, (txDB) => {
+            txDB.create('Post', {
+                content: 'content',
+                cid: 'cid',
+                epochKey: postEpochKey,
+                epoch: epoch,
+                transactionHash: 'txnHash',
+                status: 0,
+            })
+            return 1
+        })
+    })
+
+    after(async function () {
+        await ethers.provider.send('evm_revert', [snapshot])
+        express.close()
+    })
+
+    it('should fetch posts', async function () {
+        const posts: any = await fetch(
+            `${HTTP_SERVER}/api/my-account/posts?epks=${postEpochKey}`,
+        ).then((r) => {
+            expect(r.status).equal(200)
+            return r.json()
+        })
+
+        expect(posts.length).equal(1)
+    })
+})
