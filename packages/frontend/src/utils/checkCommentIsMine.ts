c03c1263ed5cb1066655ee828a42ad09b7666a89
badukwei <linwgpeter@gmail.com>
2023-12-18 23:14:37 +0800

    feat: useFetchComment test

--- packages/frontend/src/utils/checkCommentIsMine.ts
+++ packages/frontend/src/utils/checkCommentIsMine.ts
@@ -5,8 +5,9 @@ const checkCommentIsMine = (
     comment: CommnetDataFromApi,
     userState: UserState
 ) => {
+    if (!userState.getEpochKeys(comment.epoch)) return false
+    
     const epochKeys = userState.getEpochKeys(comment.epoch).toString()
-
     if (epochKeys.includes(comment.epochKey)) return true
 
     return false
