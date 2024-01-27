05c997a35e3c2fff47f084fdc68b0b89de34478a
badukwei <linwgpeter@gmail.com>
2024-01-27 15:35:00 +0800

    fix:format time

--- packages/frontend/src/utils/formatDate.ts
+++ packages/frontend/src/utils/formatDate.ts
@@ -1,8 +1,11 @@
-import dayjs from 'dayjs'
+import dayjs from 'dayjs';
 
-export default function formatDate(date: number | string | Date) {
-    const publishedTime = dayjs(date)
+export default function formatDate(date: number | string | Date): string {
+    let dateToFormat = typeof date === 'string' ? parseInt(date, 10) : date;
+
+    const publishedTime = dayjs(dateToFormat);
+
     return publishedTime.isBefore(dayjs(), 'day')
         ? publishedTime.format('YYYY/MM/DD')
-        : publishedTime.fromNow()
+        : publishedTime.fromNow();
 }
