504dd2dee715fbafe31e5814363dfd088b353401
xiawpohr <xiawpohr@gmail.com>
2024-05-10 17:16:43 +0800

    fix getEpochKeyNonce test

--- packages/frontend/src/__tests__/utils/getEpochKeyNonce.test.ts
+++ packages/frontend/src/__tests__/utils/getEpochKeyNonce.test.ts
@@ -2,13 +2,13 @@ import { getEpochKeyNonce } from '@/utils/getEpochKeyNonce'
 
 describe('getEpochKeyNonce', () => {
     it('returns actionCount when it is less than NUM_EPOCH_KEY_NONCE_PER_EPOCH', () => {
-        expect(getEpochKeyNonce(1)).toBe(0)
-        expect(getEpochKeyNonce(2)).toBe(1)
-        expect(getEpochKeyNonce(3)).toBe(2)
+        expect(getEpochKeyNonce(0)).toBe(0)
+        expect(getEpochKeyNonce(1)).toBe(1)
+        expect(getEpochKeyNonce(2)).toBe(2)
     })
 
     it('returns a random number when actionCount is greater than or equal to NUM_EPOCH_KEY_NONCE_PER_EPOCH', () => {
         jest.spyOn(Math, 'random').mockReturnValue(0.6)
-        expect(getEpochKeyNonce(4)).toBe(1)
+        expect(getEpochKeyNonce(3)).toBe(1)
     })
 })
