dc4786f5daddb812894ff0828fb610a1d1ef5913
ryanycw <ryan@ryanycw.dev>
2024-05-14 22:38:46 +0800

    refactor: improve consistency in post relay returns

--- packages/relay/test/post.test.ts
+++ packages/relay/test/post.test.ts
@@ -89,7 +89,7 @@ describe('POST /post', function () {
                 return res.body
             })
 
-        await ethers.provider.waitForTransaction(res.transaction)
+        await ethers.provider.waitForTransaction(res.txHash)
         await sync.waitForSync()
 
         let posts = await express
@@ -99,7 +99,7 @@ describe('POST /post', function () {
                 return res.body
             })
 
-        expect(posts[0].transactionHash).equal(res.transaction)
+        expect(posts[0].transactionHash).equal(res.txHash)
         expect(posts[0].content).equal(testContent)
         expect(posts[0].status).equal(1)
 
@@ -241,7 +241,7 @@ describe('POST /post', function () {
 
     it('should fetch posts which are already on-chain', async function () {
         // send a post
-        const { transaction } = await post(express, userState)
+        const { txHash } = await post(express, userState)
         // update the cache, the amount of posts is still 10
         // since the above post is not on-chain yet
         await pService.updateOrder(sqlite)
@@ -269,7 +269,7 @@ describe('POST /post', function () {
         // check the post is off-chain so the status must be 0
         const offChainPost = await sqlite.findOne('Post', {
             where: {
-                transactionHash: transaction,
+                transactionHash: txHash,
             },
         })
 
