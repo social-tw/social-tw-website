8c20e0fc93893d9e7ab6a5a6a7d8a384fccd8986
xiawpohr <xiawpohr@gmail.com>
2024-05-14 23:36:31 +0800

    chore: remove unused dependencies

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -3,9 +3,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    testEnvironmentOptions: {
-        url: 'http://localhost:8000',
-    },
     moduleNameMapper: {
         '@testing-library/react':
             '<rootDir>/node_modules/@testing-library/react',
