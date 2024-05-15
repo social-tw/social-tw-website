2eca8eae842270616b7cf619ccc1fb5683dc80a2
UranusLin <aka611511@gmail.com>
2024-05-15 21:04:18 +0800

    feat: fix for merge dev

--- packages/frontend/src/hooks/useErrorMessage.ts
+++ packages/frontend/src/hooks/useErrorMessage.ts
@@ -1,14 +1,11 @@
 import { useMemo } from 'react'
 import ERROR_MESSAGES from '../constants/error-messages/errorMessage'
 
-const useErrorMessage = (errorCode: keyof typeof ERROR_MESSAGES | '') => {
 export default function useErrorMessage(
-    errorCode: keyof typeof LOGIN_ERROR_MESSAGES | '',
+    errorCode: keyof typeof ERROR_MESSAGES | '',
 ) {
-    const errorMessage = useMemo(() => {
+    return useMemo(() => {
         if (!errorCode) return { code: 'NO_ERROR', message: '' }
         return ERROR_MESSAGES[errorCode]
     }, [errorCode])
-
-    return errorMessage
 }
