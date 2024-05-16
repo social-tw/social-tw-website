06244648be52f62e18d01118a37fa73ea80395f7
xiawpohr <xiawpohr@gmail.com>
2024-05-16 16:30:04 +0800

    test: useLoginWithServer

---
+++ packages/frontend/src/hooks/useLoginWithServer/useLoginWithServer.test.ts
@@ -0,0 +1,21 @@
+import { act, renderHook, waitFor } from '@testing-library/react'
+import { wrapper } from "@/utils/test-helpers/wrapper";
+import { useLoginWithServer } from "./useLoginWithServer";
+
+jest.spyOn(Storage.prototype, 'removeItem');
+
+describe('useLoginWithServer', () => {
+    it('should put signMsg to signature in localStorage', async () => {
+        localStorage.setItem('signMsg', '\"0xsignature\"')
+        const { result } = renderHook(useLoginWithServer, { wrapper })
+
+        await act(async () => {
+          await result.current.login()
+        })
+
+        const signature = localStorage.getItem('signature')
+    
+        expect(signature).toBe('\"0xsignature\"')
+        expect(localStorage.removeItem).toHaveBeenCalledWith('signMsg')
+    })
+})
