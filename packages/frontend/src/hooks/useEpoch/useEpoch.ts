1a4bc6e90285b54e2892adef3adfa4ecb2870807
xiawpohr <xiawpohr@gmail.com>
2024-05-16 17:51:29 +0800

    test: useEpoch

---
+++ packages/frontend/src/hooks/useEpoch/useEpoch.ts
@@ -0,0 +1,74 @@
+import { useEffect, useMemo, useState } from 'react'
+import { useUserState } from '../useUserState/useUserState'
+import { QueryKeys } from '@/constants/queryKeys'
+import { useQuery } from '@tanstack/react-query'
+
+const epochLength = 300
+
+export function useEpoch() {
+    const { isPending: isUserStatePending, userState } = useUserState()
+
+    const {
+        isPending: isCurrentEpochPending,
+        data: currentEpoch,
+        refetch: refetchCurrentEpoch
+    } = useQuery({
+        queryKey: [QueryKeys.CurrentEpoch, userState],
+        queryFn: async () => {
+            if (!userState) { 
+                return null
+            }
+            return userState.sync.calcCurrentEpoch()
+        }
+    })
+
+    const {
+        isPending: isRemainingTimePending,
+        data: remainingTime,
+        refetch: refetchRemainingTime
+    } = useQuery({
+        queryKey: [QueryKeys.EpochRemainingTime, userState],
+        queryFn: async () => {
+            if (!userState) {
+                return null
+            }
+            return userState.sync.calcEpochRemainingTime()
+        }
+    })
+
+    const isPending = isUserStatePending || isCurrentEpochPending || isRemainingTimePending
+
+    const epochStartTime = useMemo(
+        () =>
+            currentEpoch && remainingTime
+                ? Date.now() / 1000 - (epochLength - remainingTime)
+                : 0,
+        [currentEpoch, remainingTime],
+    )
+    const epochEndTime = useMemo(
+        () => (currentEpoch && remainingTime ? Date.now() / 1000 + remainingTime : 0),
+        [currentEpoch, remainingTime],
+    )
+
+    useEffect(() => {
+        if (currentEpoch && remainingTime) {
+            const timer = setTimeout(async () => {
+                await refetchCurrentEpoch()
+                await refetchRemainingTime()
+            }, remainingTime)
+
+            return () => {
+                clearTimeout(timer)
+            }
+        }
+    }, [currentEpoch, remainingTime])
+
+    return {
+        isPending,
+        currentEpoch,
+        remainingTime,
+        epochLength,
+        epochStartTime,
+        epochEndTime,
+    }
+}
