34b74261e1fb3d7b272df66e9fb934b30610261c
xiawpohr <xiawpohr@gmail.com>
2024-05-16 17:02:11 +0800

    test: useSignupWithWallet

---
+++ packages/frontend/src/hooks/useSignupWithWallet/useSignupWithWallet.test.ts
@@ -0,0 +1,72 @@
+import nock from 'nock'
+import { act, renderHook } from '@testing-library/react'
+import { wrapper } from "@/utils/test-helpers/wrapper"
+import { useSignupWithWallet } from "./useSignupWithWallet"
+import { SERVER } from '@/config'
+
+jest.spyOn(Storage.prototype, 'removeItem');
+
+jest.mock('@/hooks/useWeb3Provider/useWeb3Provider', () => ({
+    useWeb3Provider: () => ({
+        getGuaranteedProvider: () => ({
+            waitForTransaction: jest.fn().mockResolvedValue({
+                logs: [
+                    {
+                        topics: ['', '', '', '1111'],
+                    },
+                ],
+            }),
+        }),
+    }),
+}))
+
+jest.mock('@/hooks/useUserState/useUserState', () => ({
+    useUserState: () => ({
+        getGuaranteedUserState: () => ({
+            waitForSync: jest.fn(),
+            genUserSignUpProof: jest.fn().mockResolvedValue({
+                publicSignals: 'mocked_signals',
+                proof: 'mocked_proof',
+                epoch: 0,
+                epochKey: 'mocked_epockKey',
+            }),
+            sync: {
+                calcCurrentEpoch: jest.fn().mockReturnValue(2),
+            },
+        }),
+    }),
+}))
+
+describe('useSignupWithWallet', () => {
+    afterEach(() => {
+        nock.restore()
+        jest.clearAllMocks()
+    })
+
+    it('should signup using hashUserId and token in localStorage', async () => {
+        const expectation = nock(SERVER).post('/api/signup').reply(200, { status: 'success', hash: '0xhash'})
+        ;(window as any).ethereum = {
+          request: jest.fn().mockImplementation(async (payload) => {
+          const { method } = payload
+          switch (method) {
+              case 'eth_requestAccounts': {
+              return ['0xaccount1', '0xaccount2']
+              }
+              case 'personal_sign': {
+              return '0xsignature'
+              }
+          }
+          })
+      }
+        localStorage.setItem('hashUserId', '\"100\"')
+        localStorage.setItem('token', '\"token\"')
+        const { result } = renderHook(useSignupWithWallet, { wrapper })
+
+        await act(async () => {
+            await result.current.signup()
+        })
+
+        expect(result.current.error).toBeFalsy()
+        expectation.done()
+    })
+})
--- packages/frontend/src/hooks/useSignupWithWallet/useSignupWithWallet.ts
+++ packages/frontend/src/hooks/useSignupWithWallet/useSignupWithWallet.ts
@@ -1,9 +1,10 @@
 import { useNavigate } from 'react-router-dom'
 import { useMutation } from '@tanstack/react-query'
-import useSignup from '@/hooks/useSignup'
-import { useLoginWithWallet } from '@/hooks/useLoginWithWallet'
+import { useSignup } from '@/hooks/useSignup/useSignup'
+import { useLoginWithWallet } from '@/hooks/useLoginWithWallet/useLoginWithWallet'
 import { MutationKeys } from '@/constants/queryKeys'
 import { LocalStorageHelper } from '@/utils/LocalStorageHelper'
+import { PATHS } from '@/constants/paths'
 
 export function useSignupWithWallet() {
     const navigate = useNavigate()
@@ -22,16 +23,13 @@ export function useSignupWithWallet() {
             const hashUserId = LocalStorageHelper.getGuaranteedHashUserId()
             const accessToken = LocalStorageHelper.getGuaranteedAccessToken()
 
-            await login()
-            setTimeout(() => {
-                baseSignup({ hashUserId, accessToken, fromServer: false })
-            }, 0)
+            await baseSignup({ hashUserId, accessToken, fromServer: false })
         },
-        onMutate: () => {
-            navigate('/')
+        onMutate: async () => {
+            await login()
         },
         onError: () => {
-            navigate('/welcome')
+            navigate(PATHS.WELCOME)
         },
     })
 
