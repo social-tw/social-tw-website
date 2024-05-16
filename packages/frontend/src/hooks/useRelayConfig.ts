ff9656f0177ce6909d43c8d1e49101834ced4147
xiawpohr <xiawpohr@gmail.com>
2024-05-16 16:27:43 +0800

    test: useUserState

--- packages/frontend/src/hooks/useRelayConfig.ts
+++
@@ -1,9 +0,0 @@
-import { fetchRelayConfig } from '@/utils/api'
-import { useQuery } from '@tanstack/react-query'
-
-export default function useRelayConfig() {
-    return useQuery({
-        queryKey: ['relayConfig'],
-        queryFn: fetchRelayConfig,
-    })
-}
