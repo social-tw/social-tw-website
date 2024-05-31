435d4afadb38caf689712d3b35e144be16dfe30c
ryanycw <ryan@ryanycw.dev>
2024-05-31 23:27:24 +0800

    fix: adjust server env var naming

--- packages/frontend/src/constants/config.ts
+++ packages/frontend/src/constants/config.ts
@@ -1,3 +1,3 @@
-export const SERVER = process.env.SERVER ?? 'http://localhost:8000'
+export const SERVER = process.env.REACT_APP_SERVER ?? 'http://localhost:8000'
 export const KEY_SERVER = `${SERVER}/build/`
 export const NUM_EPOCH_KEY_NONCE_PER_EPOCH = 3
