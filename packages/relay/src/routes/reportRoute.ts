88594ca29d853f15fd460ba54a45eec0edc4523e
ryanycw <ryan@ryanycw.dev>
2024-06-23 18:08:56 +0800

    build: implement create report

--- packages/relay/src/routes/reportRoute.ts
+++ packages/relay/src/routes/reportRoute.ts
@@ -1,40 +1,30 @@
 import { DB } from 'anondb/node'
 import { Express, Request, Response } from 'express'
+import { UnirepSocialSynchronizer } from '../services/singletons/UnirepSocialSynchronizer'
 import { errorHandler } from '../services/utils/ErrorHandler'
-import { commentService } from '../services/CommentService'
-import { postService } from '../services/PostService'
 import { reportService } from '../services/ReportService'
-import { ReportHistory, ReportType } from '../types/Report'
-import {
-    InvalidPostIdError,
-    CommentNotExistError,
-} from '../types/InternalError'
 
-export default (app: Express, db: DB) => {
+export default (
+    app: Express,
+    db: DB,
+    synchronizer: UnirepSocialSynchronizer
+) => {
     app.post(
         '/api/report/create',
         errorHandler(async (req: Request, res: Response) => {
-            const reportData = req.body as ReportHistory
+            const { _reportData, publicSignals, proof } = req.body
             // 1. Validate request body
-            // 1.a Check if the post / comment exists is not reported already(post status = 1 / comment status = 1)
-            if (reportData.type === ReportType.Post) {
-                const post = await postService.fetchSinglePost(
-                    reportData.objectId.toString(),
-                    db
-                )
-                if (!post) throw InvalidPostIdError
-            } else if (reportData.type === ReportType.Comment) {
-                const comment = await commentService.fetchSingleComment(
-                    reportData.objectId.toString(),
-                    db
-                )
-                if (!comment) throw CommentNotExistError
-            }
-            // 1.b Check if the epoch key is valid
-
+            const reportData = await reportService.verifyReportData(
+                db,
+                _reportData,
+                publicSignals,
+                proof,
+                synchronizer
+            )
             // 2. Create a report
             const reportId = await reportService.createReport(db, reportData)
             // 3. Adjust Post / Comment Status
+            await reportService.updateObjectStatus(db, reportData)
             res.json({ reportId })
         })
     )
--- packages/relay/src/services/ReportService.ts
+++ packages/relay/src/services/ReportService.ts
@@ -1,12 +1,53 @@
 import { DB } from 'anondb'
+import { PublicSignals, Groth16Proof } from 'snarkjs'
 import { nanoid } from 'nanoid'
-import { ReportHistory } from '../types/Report'
+import { UnirepSocialSynchronizer } from './singletons/UnirepSocialSynchronizer'
+import { commentService } from '../services/CommentService'
+import { postService } from '../services/PostService'
+import { ReportHistory, ReportType } from '../types/Report'
+import {
+    InvalidPostIdError,
+    CommentNotExistError,
+} from '../types/InternalError'
+import ProofHelper from './utils/ProofHelper'
 
 export class ReportService {
+    async verifyReportData(
+        db: DB,
+        reportData: ReportHistory,
+        publicSignals: PublicSignals,
+        proof: Groth16Proof,
+        synchronizer: UnirepSocialSynchronizer
+    ): Promise<ReportHistory> {
+        // 1.a Check if the post / comment exists is not reported already(post status = 1 / comment status = 1)
+        if (reportData.type === ReportType.Post) {
+            const post = await postService.fetchSinglePost(
+                reportData.objectId.toString(),
+                db
+            )
+            if (!post) throw InvalidPostIdError
+            reportData.respondentEpochKey = post.epochKey
+        } else if (reportData.type === ReportType.Comment) {
+            const comment = await commentService.fetchSingleComment(
+                reportData.objectId.toString(),
+                db
+            )
+            if (!comment) throw CommentNotExistError
+            reportData.respondentEpochKey = comment.epochKey
+        }
+        // 1.b Check if the epoch key is valid
+        await ProofHelper.getAndVerifyEpochKeyProof(
+            publicSignals,
+            proof,
+            synchronizer
+        )
+        return reportData
+    }
+
     async createReport(db: DB, reportData: ReportHistory): Promise<string> {
         const reportId = nanoid()
         await db.create('ReportHistory', {
-            reportId,
+            reportId: reportId,
             type: reportData.type,
             objectId: reportData.objectId,
             reportorEpochKey: reportData.reportorEpochKey,
@@ -20,9 +61,16 @@ export class ReportService {
             category: reportData.category,
             reportEpoch: reportData.reportEpoch,
             reportAt: reportData.reportAt ?? (+new Date()).toString(),
-            _id: nanoid(),
         })
         return reportId
+    }
+
+    async updateObjectStatus(db: DB, reportData: ReportHistory) {
+        if (reportData.type === ReportType.Post) {
+            postService.updatePostStatus(reportData.objectId, 2, db)
+        } else if (reportData.type === ReportType.Comment) {
+            commentService.updateCommentStatus(reportData.objectId, 3, db)
+        }
     }
 }
 
