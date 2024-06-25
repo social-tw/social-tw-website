966ef34422635bbe72c31a58469da6eb44b3ea4d
Dylan Yang <dylanyang0523@gmail.com>
2024-06-25 22:01:00 +0900

    feat(fe): update comment report dialog

--- packages/frontend/src/features/post/components/Comment/CommentReportDialog.tsx
+++ packages/frontend/src/features/post/components/Comment/CommentReportDialog.tsx
@@ -1,7 +1,10 @@
 import { Dialog } from '@/features/shared'
-import { useForm } from 'react-hook-form'
+import { FieldValues, UseFormHandleSubmit } from 'react-hook-form'
 import { useReportComment } from '../../hooks/useReportComment'
+import { useReportForm } from '../../hooks/useReportForm'
 import {
+    REGISTER_ID_DESC,
+    REGISTER_ID_REASON,
     ReportFormCtn,
     ReportFormDesc,
     ReportFormIntro,
@@ -9,6 +12,9 @@ import {
     ReportFormStepGroup,
     ReportFormStepLabel,
     ReportFormSubmitBtn,
+    ReportFormSubmitFailure,
+    ReportFormSubmitSuccess,
+    ReportFormSubmitting,
 } from '../ReportForm'
 
 interface CommentReportDialogProps {
@@ -16,41 +22,119 @@ interface CommentReportDialogProps {
     onClose: () => void
 }
 
+const defaultValues = {
+    [`${REGISTER_ID_REASON}`]: -1,
+    [`${REGISTER_ID_DESC}`]: '',
+}
+
 export function CommentReportDialog({
     isOpen,
     onClose,
 }: CommentReportDialogProps) {
     const {
-        register,
         handleSubmit,
+        register,
+        errors,
         setValue,
         getValues,
         trigger,
-        formState: { errors },
-    } = useForm()
+        isSubmitFailure,
+        isSubmitting,
+        isSubmitSuccess,
+        isNotSubmitted,
+        resetToCurrentState,
+        resetAll,
+        updateStateToFailure,
+        updateStateToSuccess,
+        updateStateToSubmitting,
+    } = useReportForm(defaultValues)
 
-    const { report } = useReportComment()
+    const onSubmit = useSubmitCommentFlow({
+        handleSubmit,
+        updateStateToSubmitting,
+        updateStateToFailure,
+        updateStateToSuccess,
+    })
+
+    const onCloseSuccess = useCloseSuccessDialogFlow({ resetAll, onClose })
 
     return (
-        <Dialog isOpen={isOpen} onClose={onClose}>
-            <ReportFormCtn onSubmit={handleSubmit(report)}>
-                <ReportFormIntro />
-                <ReportFormStepGroup>
-                    <ReportFormStepLabel title="1. 檢舉原因" isRequired />
-                    <ReportFormReasons
-                        register={register}
-                        errors={errors}
-                        setValue={setValue}
-                        getValues={getValues}
-                        trigger={trigger}
-                    />
-                </ReportFormStepGroup>
-                <ReportFormStepGroup>
-                    <ReportFormStepLabel title="2. 檢舉描述" isRequired />
-                    <ReportFormDesc register={register} errors={errors} />
-                </ReportFormStepGroup>
-                <ReportFormSubmitBtn />
-            </ReportFormCtn>
-        </Dialog>
+        <>
+            {isNotSubmitted && (
+                <Dialog isOpen={isOpen} onClose={onClose}>
+                    <ReportFormCtn onSubmit={onSubmit}>
+                        <ReportFormIntro />
+                        <ReportFormStepGroup>
+                            <ReportFormStepLabel
+                                title="1. 檢舉原因"
+                                isRequired
+                            />
+                            <ReportFormReasons
+                                register={register}
+                                errors={errors}
+                                setValue={setValue}
+                                getValues={getValues}
+                                trigger={trigger}
+                            />
+                        </ReportFormStepGroup>
+                        <ReportFormStepGroup>
+                            <ReportFormStepLabel
+                                title="2. 檢舉描述"
+                                isRequired
+                            />
+                            <ReportFormDesc
+                                register={register}
+                                errors={errors}
+                            />
+                        </ReportFormStepGroup>
+                        <ReportFormSubmitBtn />
+                    </ReportFormCtn>
+                </Dialog>
+            )}
+            {isSubmitting && <ReportFormSubmitting />}
+            {isSubmitFailure && (
+                <ReportFormSubmitFailure onClose={resetToCurrentState} />
+            )}
+            {isSubmitSuccess && (
+                <ReportFormSubmitSuccess onClose={onCloseSuccess} />
+            )}
+        </>
     )
+}
+
+function useSubmitCommentFlow({
+    handleSubmit,
+    updateStateToSubmitting,
+    updateStateToFailure,
+    updateStateToSuccess,
+}: {
+    handleSubmit: UseFormHandleSubmit<FieldValues>
+    updateStateToSubmitting: () => void
+    updateStateToFailure: () => void
+    updateStateToSuccess: () => void
+}) {
+    const { report } = useReportComment()
+    const reportFlow = async (data: any) => {
+        try {
+            updateStateToSubmitting()
+            await report(data)
+            updateStateToSuccess()
+        } catch (e) {
+            updateStateToFailure()
+        }
+    }
+    return handleSubmit(reportFlow)
+}
+
+function useCloseSuccessDialogFlow({
+    resetAll,
+    onClose,
+}: {
+    resetAll: () => void
+    onClose: () => void
+}) {
+    return () => {
+        resetAll()
+        onClose()
+    }
 }
--- packages/frontend/src/features/post/hooks/useReportComment/index.ts
+++ packages/frontend/src/features/post/hooks/useReportComment/index.ts
@@ -1,6 +1,8 @@
 export function useReportComment() {
-    const report = () => {
-        console.log('call relay api to report comment')
+    const report = async (data: any) => {
+        console.log('call report comment api with data: ', data)
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000))
+        console.log('pretend successful report')
     }
     return { report }
 }
