19464e8bbb3cca0b6aabd2296cb29fb744ac76d5
Tihe <tihechen@gmail.com>
2024-06-26 17:14:12 +0800

    increate max fee per gas for L1 use

--- packages/relay/src/config.ts
+++ packages/relay/src/config.ts
@@ -61,5 +61,5 @@ export const UPDATE_POST_ORDER_INTERVAL = 0.5 * 60 * 60 * 1000
 export const DAY_DIFF_STAEMENT = DB_PATH.startsWith('postgres')
     ? '(EXTRACT (DAY FROM NOW()::timestamp - TO_TIMESTAMP(publishedAt::bigint / 1000)::date))'
     : "FLOOR(JULIANDAY('now') - JULIANDAY(DATETIME(CAST(publishedAt AS INTEGER) / 1000, 'unixepoch')))"
-
-export const MAX_FEE_PER_GAS = ethers.utils.parseUnits('0.1', 'gwei')
+// L1 gasFee is higher so cannot set 0.1 gwei, otherwise the test won't pass
+export const MAX_FEE_PER_GAS = ethers.utils.parseUnits('10', 'gwei')
