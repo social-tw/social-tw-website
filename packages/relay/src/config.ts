b74b4ddb45beea20766445e2d2adde0d3bc09deb
ryanycw <ryan@ryanycw.dev>
2024-07-02 23:48:50 +0800

    chore: fix test

--- packages/relay/src/config.ts
+++ packages/relay/src/config.ts
@@ -16,7 +16,7 @@ Object.assign(process.env, {
         '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
     GENESIS_BLOCK: process.env.GENESIS_BLOCK ?? '0',
     REPORT_SETTLE_VOTE_THRESHOLD:
-        process.env.REPORT_SETTLE_VOTE_THRESHOLD ?? '1',
+        process.env.REPORT_SETTLE_VOTE_THRESHOLD ?? '5',
     ...process.env,
 })
 
