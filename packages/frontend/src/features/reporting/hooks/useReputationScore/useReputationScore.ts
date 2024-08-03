6c1f14fbc15fe4e468d5884f287035e2641d70b1
Dylan Yang <dylanyang0523@gmail.com>
2024-08-03 13:38:48 +0900

    feat: useReputationScore

--- packages/frontend/src/features/reporting/hooks/useReputationScore/useReputationScore.ts
+++ packages/frontend/src/features/reporting/hooks/useReputationScore/useReputationScore.ts
@@ -1,11 +1,15 @@
 import { QueryKeys } from '@/constants/queryKeys'
+import { useUserState } from '@/features/core'
 import { useQuery } from '@tanstack/react-query'
 
 export function useReputationScore() {
+    const { getGuaranteedUserState } = useUserState()
     const { data: reputationScore } = useQuery({
         queryKey: [QueryKeys.ReputationScore],
         queryFn: async () => {
-            return 10
+            const userState = await getGuaranteedUserState()
+            const data = await userState.getData()
+            return Number(data[0].toString()) + 1
         },
     })
 
