39bafff06f7e4e7276dd96299ede35d23c968a60
xiawpohr <xiawpohr@gmail.com>
2024-09-25 17:55:24 +0800

    feat: update notification position

--- packages/frontend/src/routes/app/layout.tsx
+++ packages/frontend/src/routes/app/layout.tsx
@@ -33,7 +33,7 @@ function NotificationContainer({ children }: { children: React.ReactNode }) {
     return (
         <div
             id="notifications"
-            className="fixed z-20 right-4 bottom-28 lg:left-4 lg:right-auto lg:bottom-20"
+            className="fixed z-20 right-4 bottom-28 lg:left-10 lg:right-auto lg:bottom-20"
         >
             {children}
         </div>
