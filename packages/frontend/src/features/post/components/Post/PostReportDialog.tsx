94beedee5a1aec619bfc2ea521e2114c870d5808
badukwei <linwgpeter@gmail.com>
2024-11-06 20:19:36 +0800

    refactor: optimize code

--- packages/frontend/src/features/post/components/Post/PostReportDialog.tsx
+++ packages/frontend/src/features/post/components/Post/PostReportDialog.tsx
@@ -1,9 +1,10 @@
 import ReportContent from '@/features/reporting/components/Adjudicate/ReportContent'
-import { Dialog } from '@/features/shared'
+
 import { useState } from 'react'
 import { FieldValues, useForm, UseFormGetValues } from 'react-hook-form'
 import { useReportPost } from '../../hooks/useReportPost/useReportPost'
 import {
+    InfoDialog,
     REGISTER_ID_DESC,
     REGISTER_ID_REASON,
     ReportFormCategories,
@@ -17,6 +18,7 @@ import {
     ReportFormSubmitSuccess,
     ReportFormSubmitting,
 } from '../ReportForm'
+import Dialog from '@/features/shared/components/Dialog/Dialog'
 
 interface PostReportDialogProps {
     postId: string
@@ -67,6 +69,10 @@ export function PostReportDialog({
         } catch (error) {}
     })
 
+    const resetStep = () => {
+        setStep(1)
+    }
+
     const onFailureResubmit = useFailureResubmitDialogFlow({
         resetForm,
         resetState,
@@ -77,44 +83,17 @@ export function PostReportDialog({
         resetForm,
         resetState,
         onClose,
+        resetStep,
     })
 
     return (
         <>
             {step === 1 && (
-                <Dialog isOpen={isOpen} onClose={onCloseDialog}>
-                    <div className="py-6 px-12 flex flex-col items-center justify-between gap-16">
-                        <p className="mt-2 tracking-wide leading-7">
-                            親愛的用戶：
-                            <br />
-                            <br />
-                            Unirep Social
-                            Taiwan為一去中心化的用戶自治管理社群平台，藉由透過檢舉不當內容的方式來
-                            <span className="font-bold text-secondary">
-                                維持社群的安全與健康
-                            </span>
-                            。
-                            檢舉的流程會需要你選擇檢舉原因類別、填寫檢舉原因描述，並送出申請。
-                            <span className="font-bold text-secondary">
-                                提交的檢舉案將交由平台上的 5
-                                位隨機用戶進行審核評判
-                            </span>
-                            。若檢舉案審核評判通過，你的聲譽分數會加 3
-                            分，反之，則你的聲譽分數降 1 分。
-                            <span className="font-bold text-secondary">
-                                因此請審慎填寫檢舉案的內容
-                            </span>
-                            ，以幫助其他用戶進行評判，並避免聲譽分數被降低的風險。
-                        </p>
-                        <button
-                            className="w-80 h-14 rounded-lg text-white/90 flex justify-center items-center text-xl font-bold tracking-[30%] bg-primary/90"
-                            type="button"
-                            onClick={() => setStep(2)}
-                        >
-                            確認檢舉
-                        </button>
-                    </div>
-                </Dialog>
+                <InfoDialog
+                    isOpen={isOpen}
+                    onClose={onCloseDialog}
+                    onButtonClick={() => setStep(2)}
+                />
             )}
 
             {step === 2 && isIdle && (
@@ -166,15 +145,18 @@ function useCloseDialogFlow({
     resetForm,
     resetState,
     onClose,
+    resetStep,
 }: {
     resetForm: () => void
     resetState: () => void
     onClose: () => void
+    resetStep: () => void
 }) {
     return () => {
         resetForm()
         resetState()
         onClose()
+        resetStep()
     }
 }
 
---
+++ packages/frontend/src/features/post/components/ReportForm/InfoDialog.tsx
@@ -0,0 +1,48 @@
+import { Dialog } from '@/features/shared'
+
+interface InfoDialogProps {
+    isOpen: boolean
+    onClose: () => void
+    onButtonClick: () => void
+}
+
+export function InfoDialog({
+    isOpen,
+    onClose,
+    onButtonClick,
+}: InfoDialogProps) {
+    return (
+        <Dialog isOpen={isOpen} onClose={onClose}>
+            <div className="py-6 px-12 flex flex-col items-center justify-between gap-16">
+                <p className="mt-2 tracking-wide leading-7">
+                    親愛的用戶：
+                    <br />
+                    <br />
+                    Unirep Social
+                    Taiwan為一去中心化的用戶自治管理社群平台，藉由透過檢舉不當內容的方式來
+                    <span className="font-bold text-secondary">
+                        維持社群的安全與健康
+                    </span>
+                    。
+                    檢舉的流程會需要你選擇檢舉原因類別、填寫檢舉原因描述，並送出申請。
+                    <span className="font-bold text-secondary">
+                        提交的檢舉案將交由平台上的 5 位隨機用戶進行審核評判
+                    </span>
+                    。若檢舉案審核評判通過，你的聲譽分數會加 3
+                    分，反之，則你的聲譽分數降 1 分。
+                    <span className="font-bold text-secondary">
+                        因此請審慎填寫檢舉案的內容
+                    </span>
+                    ，以幫助其他用戶進行評判，並避免聲譽分數被降低的風險。
+                </p>
+                <button
+                    className="w-80 h-14 rounded-lg text-white/90 flex justify-center items-center text-xl font-bold tracking-[30%] bg-primary/90"
+                    type="button"
+                    onClick={onButtonClick}
+                >
+                    確認檢舉
+                </button>
+            </div>
+        </Dialog>
+    )
+}
