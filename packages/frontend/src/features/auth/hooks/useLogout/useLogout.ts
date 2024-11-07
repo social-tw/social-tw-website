b1a2f02b46e53a95d2ce061f652e1f1846a30271
chen-yanlong <alan900801@gmail.com>
2024-11-07 20:57:46 +0800

    feat: clear notification after logout

--- packages/frontend/src/features/auth/hooks/useLogout/useLogout.ts
+++ packages/frontend/src/features/auth/hooks/useLogout/useLogout.ts
@@ -3,6 +3,7 @@ import { useMutation } from '@tanstack/react-query'
 import { LocalStorageHelper } from '@/utils/helpers/LocalStorageHelper'
 import { MutationKeys } from '@/constants/queryKeys'
 import { PATHS } from '@/constants/paths'
+import NotificationService from '@/features/notification/services/NotificationService'
 
 export function useLogout() {
     const navigate = useNavigate()
@@ -21,6 +22,7 @@ export function useLogout() {
             LocalStorageHelper.removeSignMsg()
         },
         onSuccess: () => {
+            NotificationService.clearNotifications()
             navigate(PATHS.WELCOME)
         },
     })
