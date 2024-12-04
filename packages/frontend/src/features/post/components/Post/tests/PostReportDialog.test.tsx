81264778f075609029a6ee2e19afc089b7880e85
badukwei <linwgpeter@gmail.com>
2024-12-04 23:46:08 +0800

    fix: minor fix

--- packages/frontend/src/features/post/components/Post/tests/PostReportDialog.test.tsx
+++ packages/frontend/src/features/post/components/Post/tests/PostReportDialog.test.tsx
@@ -28,22 +28,6 @@ describe('PostReportDialog', () => {
             <PostReportDialog postId={''} isOpen={true} onClose={() => {}} />,
             { wrapper },
         )
-        expect(
-            screen.getByText(
-                '若您確定要檢舉此內容，請選擇以下欄位中的原因，並填寫詳盡的檢舉描述。填寫完成後，您即可提交檢舉。',
-            ),
-        ).toBeInTheDocument()
-    })
-
-    it('should render isPending content', () => {
-        const mockUseReportPost = useReportPost as jest.MockedFunction<
-            typeof useReportPost
-        >
-        mockUseReportPost.mockReturnValue({ ...mockValues, isPending: true })
-        render(
-            <PostReportDialog postId={''} isOpen={true} onClose={() => {}} />,
-            { wrapper },
-        )
-        expect(screen.getByText('您的檢舉報告正在送出中')).toBeInTheDocument()
+        expect(screen.getByText('確認檢舉')).toBeInTheDocument()
     })
 })
