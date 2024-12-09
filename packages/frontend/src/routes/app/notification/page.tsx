3e0c3c89603e13f3a0fa14ae8665b3ab9bede718
xiawpohr <xiawpohr@gmail.com>
2024-12-09 15:09:02 +0800

    feat: update bell iconn & adjust page styles

--- packages/frontend/src/routes/app/notification/page.tsx
+++ packages/frontend/src/routes/app/notification/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import NotificationItem from '@/features/notification/components/NotificationItem/NotificationItem'
 import { useAllNotifications } from '@/features/notification/stores/useNotificationStore'
+import React from 'react'
 
 const NotificationPage: React.FC = () => {
     const notifications = useAllNotifications()
@@ -10,22 +10,22 @@ const NotificationPage: React.FC = () => {
     }
 
     return (
-        <div className="relative px-4 py-8 md:pt-24 text-white min-h-screen">
-            <div className="absolute top-0 left-0 right-0 px-4 py-4 rounded-lg">
-                <p className="text-sm">
+        <div className="px-4 pt-5 pb-10 space-y-5 lg:px-0">
+            <section className="px-4">
+                <p className="text-xs leading-7 text-white md:text-sm md:leading-7">
                     歡迎來到通知中心，這些通知訊息僅會在你本次的登入期間中保留著。當你登出後這些訊息將清空。
                     若需要查看過去的一些上鏈交易的歷史紀錄，可以到「我的帳號」
                     {'>'}「歷史紀錄」跟「聲譽分數」的頁面中做查詢。
                 </p>
-            </div>
-            <div className="pt-24 space-y-4">
+            </section>
+            <section className="space-y-4">
                 {[...notifications].reverse().map((notification) => (
                     <NotificationItem
                         key={notification.id}
                         id={notification.id}
                     />
                 ))}
-            </div>
+            </section>
         </div>
     )
 }
