60a32eacf15f21303199bca72af0fca7d27172ef
xiawpohr <xiawpohr@gmail.com>
2024-12-24 18:40:38 +0800

    fix: expand the community governance section in the about page

--- packages/frontend/src/features/post/components/ReportedMask/ActionLinks.tsx
+++ packages/frontend/src/features/post/components/ReportedMask/ActionLinks.tsx
@@ -4,8 +4,11 @@ interface ActionLinksProps {
 
 export default function ActionLinks({ onClick }: ActionLinksProps) {
     return (
-        <nav className="flex gap-5 self-end text-right text-xs font-medium py-3">
-            <a href="/about" className="basis-auto underline">
+        <nav className="flex self-end gap-5 py-3 text-xs font-medium text-right">
+            <a
+                href="/about?viewId=feature-community"
+                className="underline basis-auto"
+            >
                 為什麼會有內容被檢舉？
             </a>
             <button className="underline" onClick={onClick}>
